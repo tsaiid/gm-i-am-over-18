c1d0275e4f6577b8310dc0273872697c33175613
Jaxon <jaxon0523@gmail.com>
2020-02-18 22:54:07 +0800

    Update gm-i-am-over-18.user.js
    change version

--- gm-i-am-over-18.user.js
+++ gm-i-am-over-18.user.js
@@ -4,7 +4,7 @@
 // @namespace       http://tsai.it/project/gmscripts/i-am-over-18/
 // @homepageURL     https://github.com/tsaiid/gm-i-am-over-18
 // @run-at          document-idle
-// @version         0.2.20190221
+// @version         0.2.20200218
 // @description     Automatically agrees the "I'm over 18" declaration. The current supported sites are primarily in Taiwan.
 // @icon            https://raw.githubusercontent.com/tsaiid/gm-i-am-over-18/master/icon48.png
 // @icon64          https://raw.githubusercontent.com/tsaiid/gm-i-am-over-18/master/icon64.png
